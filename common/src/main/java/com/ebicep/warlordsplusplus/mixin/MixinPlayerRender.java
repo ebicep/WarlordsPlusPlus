36253d2f10028f0673b0e9fc7b3cb1f99da01316
ebicep <wmah314@gmail.com>
2023-08-08 22:48:09 -0400

    feat: added cooldown renderer rendering

---
+++ common/src/main/java/com/ebicep/warlordsplusplus/mixin/MixinPlayerRender.java
@@ -0,0 +1,29 @@
+package com.ebicep.warlordsplusplus.mixin;
+
+import com.ebicep.warlordsplusplus.event.PlayerRenderEvent;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerRenderer.class)
+public class MixinPlayerRender {
+
+    @Inject(
+            method = "render(Lnet/minecraft/client/player/AbstractClientPlayer;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
+            at = @At("HEAD")
+    )
+    public void render(AbstractClientPlayer abstractClientPlayer, float f, float g, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, CallbackInfo ci) {
+        PlayerRenderEvent.PLAYER_RENDER_POST.invoker().onPlayerRender(
+                poseStack,
+                (MultiBufferSource.BufferSource) multiBufferSource,
+                abstractClientPlayer
+        );
+    }
+
+
+}
